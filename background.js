361907581dc3456da6ea98b473f58268e59b9854
Quellensa <57103965+Quellens@users.noreply.github.com>
2020-08-08 18:35:16 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,33 @@
+let currentTab; 
+
 chrome.tabs.query(
   {currentWindow: true},
    function(tabArray){
-    let active = tabArray.filter(ob => ob.active )[0];
+   
     chrome.commands.onCommand.addListener(function(command) {
+                
      if(command == "toggle-left"){
-        console.log("left");
+
+       let indexL = (currentTab.index !=0) ? currentTab.index - 1: tabArray.length-1;   
+       let page = tabArray.filter(ob => ob.index == indexL)[0];
+       chrome.tabs.update(page.id, {active:true, highlighted: true});
+       chrome.tabs.onActivated.addListener(getCurrentTab);  
      }
-     
+    
      if(command == "toggle-right"){
-        console.log("right");  
+
+        let indexR = (currentTab.index !=tabArray.length-1) ? currentTab.index + 1: 0;   
+        let page = tabArray.filter(ob => ob.index == indexR)[0];  
+        chrome.tabs.update(page.id, {active:true, highlighted: true});
+        chrome.tabs.onActivated.addListener(getCurrentTab);  
      }
 });
 });
+
+function getCurrentTab(info){
+    chrome.tabs.get(info.tabId, function(data){
+       currentTab = data;
+    })
+}
+
+chrome.tabs.onActivated.addListener(getCurrentTab);
