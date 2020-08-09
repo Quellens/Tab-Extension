2fb505fee4ad7b92913ed7078a4b2157fcc33464
Quellensa <57103965+Quellens@users.noreply.github.com>
2020-08-09 12:00:16 +0200

    🎉  Error fixed!
    Error: When you change the tabs (moving, closing, opening), the tabArray will not be updated. Now there are no errors (hopefully)!

--- background.js
+++ background.js
@@ -1,33 +1,34 @@
 let currentTab;
+getCurrentTab();
 
-chrome.tabs.query(
-  {currentWindow: true},
-   function(tabArray){
-    currentTab = tabArray.filter(ob=>ob.active)[0];
-    chrome.commands.onCommand.addListener(function(command) {
-                
+chrome.commands.onCommand.addListener( async function(command){
+     chrome.tabs.query({currentWindow: true},async function(tabArray){  
+         
+     currentTab = tabArray.filter(ob=>ob.active)[0];
+         
      if(command == "toggle-left"){
-
-       let indexL = (currentTab.index !=0) ? currentTab.index - 1: tabArray.length-1;   
-       let page = tabArray.filter(ob => ob.index == indexL)[0];
+     
+       let indexL = await (currentTab.index > 0 && currentTab.index <= tabArray.length-1) ? currentTab.index - 1: tabArray.length-1;   
+       let page = await tabArray.filter(ob => ob.index == indexL)[0];
        chrome.tabs.update(page.id, {active:true, highlighted: true});
-       chrome.tabs.onActivated.addListener(getCurrentTab);  
+       getCurrentTab();
+       
      }
     
      if(command == "toggle-right"){
-
-        let indexR = (currentTab.index !=tabArray.length-1) ? currentTab.index + 1: 0;   
-        let page = tabArray.filter(ob => ob.index == indexR)[0];  
+         
+        let indexR = await (currentTab.index >= 0 && currentTab.index < tabArray.length-1) ? currentTab.index + 1: 0;   
+        let page = await tabArray.filter(ob => ob.index == indexR)[0];  
         chrome.tabs.update(page.id, {active:true, highlighted: true});
-        chrome.tabs.onActivated.addListener(getCurrentTab);  
-     }
-});
-});
+        getCurrentTab(); 
+         
+     }                                                                                  
+})});
 
-function getCurrentTab(info){
+function getCurrentTab(){
+chrome.tabs.onActivated.addListener((info) => {
     chrome.tabs.get(info.tabId, function(data){
-       currentTab = data;
+    currentTab = data;
     })
-}
-
-chrome.tabs.onActivated.addListener(getCurrentTab);  
+});  
+};
